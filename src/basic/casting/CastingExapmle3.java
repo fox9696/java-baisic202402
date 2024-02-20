7c23a0913d929a332209df442d7e2006867d7e61
surkanghyun <qorrnfn2000@naver.com>
2024-02-20 18:20:34 +0900

    initial commit

--- src/basic/casting/CastingExapmle3.java
+++ src/basic/casting/CastingExapmle3.java
@@ -15,8 +15,11 @@ public class CastingExapmle3 {
  
         int k = 10;
         double d = k / 4;
-       //double d = k / 4.0;  // *k좌측에 (double)안넣은이유는 작은쪽은 큰쪽을따라가서 double의 4.0을 k(int)가 따라간것
-        System.out.println("d = " + d);  //출력 :2.0 *int 값으로출력되서 int 는 소수점표현못하기에 뒤에 0.5를 버림
+       //double d = k / 4.0;  // *k좌측에 (double)안넣은이유는 작은쪽은
+        // 큰쪽을따라가서 double의 4.0을 k(int)가 따라간것
+
+        System.out.println("d = " + d);  //출력 :2.0 *int 값으로출력되서 int 는
+        // 소수점표현못하기에 뒤에 0.5를 버림
         
         
     }
