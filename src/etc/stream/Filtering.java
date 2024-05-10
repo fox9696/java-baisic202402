0b0eb8eefdc4a99ac0a0dd5d262af1b9be58ee33
surkanghyun <qorrnfn2000@naver.com>
2024-05-10 14:44:06 +0900

    #java Stream filter 연습

--- src/etc/stream/Filtering.java
+++ src/etc/stream/Filtering.java
@@ -1,6 +1,8 @@
 package etc.stream;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -27,6 +29,53 @@ public class Filtering {
                 .collect(toList())
                 .forEach(dish -> System.out.println(dish));
 
+
+
+
+        System.out.println("====== 요리중에 이름이 4글자이상인 요리만 필터링 ======");
+     menuList.stream()
+                     .filter(dish -> dish.getName().length() > 4)
+                     .collect(toList())
+                             .forEach(dish -> System.out.println(dish));
+
+
+
+
+        System.out.println("====== 요리중에 300칼로리보다 큰 요리만 필터링  ======");
+    menuList.stream()
+            .filter(dish -> dish.getCalories() >300)
+            .limit(3)  //상위 top3 추출
+            .collect(toList())
+            .forEach(d -> System.out.println(d));
+
+        System.out.println("====================================================");
+
+        menuList.stream()
+                .filter(dish -> dish.getCalories() >300)
+                .skip(2) //맨 앞 두개 제외
+                .collect(toList())
+                .forEach(d -> System.out.println(d));
+
+
+        System.out.println("====== 메뉴목록에서 처음 등장하는 생선요리2개필터링 ======");
+        menuList.stream()
+                .filter(dish -> dish.getType() == Dish.Type.FISH)
+                .limit(2)
+                .collect(toList())
+                .forEach(dish -> System.out.println(dish));
+
+
+        List<Integer> numbers = List.of(1,2,1,3,3,2,4,4,5,6);
+        //짝수만 걸러내기
+        List<Integer> filteredNumber = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .distinct() //중복제거
+                .collect(toList());
+        System.out.println("filteredNumber = " + filteredNumber);
+
+        //중복제거
+//        Set<Integer> distinctNumbers = new HashSet<>(filteredNumber);
+//        System.out.println("distinctNumbers = " + distinctNumbers);
 
 
     }
